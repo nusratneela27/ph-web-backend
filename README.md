dfa9b71df9aea56691a5e085314bbeea87150ba5
neela <nusratneela27@gmail.com>
2024-05-26 00:52:58 +0600

    inital server setup

---
+++ README.md
@@ -0,0 +1,38 @@
+## Server setup
+
+1. **First open cmd write**
+
+```bash
+   npm init -y
+```
+
+2. **Install all dependencies**
+
+```bash
+   npm install cors mongodb express dotenv
+```
+
+3. **Then run dev**
+
+```bash
+   npm run dev
+```
+
+### If clone this project just write
+
+```bash
+   npm i
+```
+
+### Deployment Steps
+
+1. **Deploy the application**:
+
+   ```bash
+    vercel
+   ```
+
+2. **Deploy the application to production**:
+   ```bash
+   vercel --prod
+   ```
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        console.log("connected to MongoDB");
+
+
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
