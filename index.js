9b09dc488834bd49ce3929d3ce745de786db9836
neela <nusratneela27@gmail.com>
2024-05-26 16:31:40 +0600

    user store in db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -19,8 +19,36 @@ async function run() {
         await client.connect();
         console.log("connected to MongoDB");
 
-        const db = client.db('job-task');
-        const recipesCollection = db.collection('recipe');
+        const usersCollection = client.db('job-task').collection('users')
+        const recipesCollection = client.db('job-task').collection('recipe');
+
+        // Save user in database
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email: email }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, option)
+            // console.log(result);
+            res.send(result)
+        })
+
+        // Get all Recipes
+        app.get("/recipes", async (req, res) => {
+            const result = await recipesCollection.find().toArray()
+            res.send(result)
+        })
+
+        // Get Single Recipe
+        app.get("/recipe/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await recipesCollection.findOne(query)
+            res.send(result)
+        })
 
         // Save recipes in database
         app.post("/add-recipes", async (req, res) => {
